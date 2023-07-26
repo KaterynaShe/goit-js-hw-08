2de26003a0c902eee19cabad426a65ce2592087a
KaterynaShe <kat2141980@gmail.com>
2023-07-26 23:03:20 +0200

    commit5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
-});
+// time from  localStorage//
+player.on('timeupdate', throttle((event) => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000));
 
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
-};
-
-player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
-
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
+//  time from localStorage continue//
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
